aef35d86194cd2f6d4478b3b311d01b1fc52414e
covapa16 <covapa16@GB2473603.uwgb.edu>
2026-03-23 13:27:40 -0500

    New unstaged change that needs to be committed

--- BoneForgeDefense/src/BoneForgeDefense/Scenes/SettingsMenuController.java
+++ BoneForgeDefense/src/BoneForgeDefense/Scenes/SettingsMenuController.java
@@ -1,42 +1,80 @@
 package BoneForgeDefense.Scenes;
 
-import javafx.animation.AnimationTimer;
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.Slider;
+import javafx.stage.Stage;
 
 public class SettingsMenuController {
-	
-	private long lastNanoSecond = 0;
-	private Boolean gameOver = false;
-	
-	public void startGameLoop() {
-        new AnimationTimer() {
-            @Override
-            public void handle(long now) {
-            	
-            	// Skip check on first frame
-                if (lastNanoSecond == 0) {
-                		lastNanoSecond = now;
-                		return; 
-                }
-                
-                // Convert nanoseconds to seconds and calculate how many have past since 
-                // last frame
-                double delta = (now - lastNanoSecond) / 1_000_000_000.0;
-                lastNanoSecond = now;
- 
-                // Stop time if Game Over bool is true
-                if (!gameOver) {
-                    update(delta);
-                }
-                
-            }
-        }.start();
-    }
-	
-	private int testCounter = 0;
-	private void update(double delta) {
-		testCounter += 1;
-		System.out.println(testCounter);
-	}
 
+    @FXML
+    private RadioButton extraLargeScreenButton;
+
+    @FXML
+    private RadioButton largeScreenButton;
+
+    @FXML
+    private RadioButton mediumScreenButton;
+
+    @FXML
+    private RadioButton smallScreenButton;
+
+    @FXML
+    private Slider volumeSlider;
+
+    @FXML
+    void handleScreenSizeChange(ActionEvent event) {
+
+    }
+    
+    public void initialize() {
+    	smallScreenButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
+    		@Override
+    		public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+    			if (newValue) {
+    				changeWindowSize(1366, 768);
+    			}
+    		}
+    	});
+    	mediumScreenButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
+    		@Override
+    		public void changed(ObservableValue<? extends Boolean>observable, Boolean oldValue, Boolean newValue) {
+    			if (newValue) {
+    				changeWindowSize(1920, 1080);
+    			}
+    		}
+    	});
+    	largeScreenButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
+    		@Override
+    		public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+    			if (newValue) {
+    				changeWindowSize(2560, 1440);
+    			}
+    		}
+    	});
+    	extraLargeScreenButton.selectedProperty().addListener(new ChangeListener<Boolean>() {
+    		@Override
+    		public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+    			if (newValue) {
+    				changeWindowSize(3840, 2160);
+    			}
+    		}
+    	});
+    }
+    
+    private Stage stage;
+    
+    public void setStage(Stage stage) {
+    	this.stage = stage;
+    }
+    
+    private void changeWindowSize(double width, double height) {
+    	if (stage != null) {
+    		stage.setWidth(width);
+    		stage.setHeight(height);
+    		stage.centerOnScreen();
+    	}
+    }
+    
 }
