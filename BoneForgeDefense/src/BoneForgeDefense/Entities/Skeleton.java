8703bd8fdf7bda36ac14351411099290ab0deb89
steve <steve@Stevens3080ti>
2026-04-13 02:27:32 -0500

    moved enemyList to skeleton superclass

--- BoneForgeDefense/src/BoneForgeDefense/Entities/Skeleton.java
+++ BoneForgeDefense/src/BoneForgeDefense/Entities/Skeleton.java
@@ -1,14 +1,20 @@
 package BoneForgeDefense.Entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class Skeleton extends Entity{
 	private double health;
+	public static List<Skeleton> enemyList = new ArrayList<Skeleton>();
 	public Skeleton(double xPos, double yPos, String spritePath) {
 		super(xPos, yPos, spritePath);
-		this.setHealth(100);
+		enemyList.add(this);
 	}
 	
 	public double getRange(double otherX, double otherY) {
-    	double distance = Math.sqrt(Math.pow(this.xPos-otherX, 2) + Math.pow(this.yPos-otherY, 2));
+		//does not sqrt the result to avoid costly calculations when called every frame. square other range values to compare to this value
+		
+    	double distance = Math.pow(this.xPos-otherX, 2) + Math.pow(this.yPos-otherY, 2);
     	return distance;
     }
 
