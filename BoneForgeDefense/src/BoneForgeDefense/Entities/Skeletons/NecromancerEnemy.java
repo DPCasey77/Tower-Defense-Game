450ead97be84c40cca2466acf494724f14dd82a2
steve <steve@Stevens3080ti>
2026-04-23 01:56:35 -0500

    added some code to get bones in range

--- BoneForgeDefense/src/BoneForgeDefense/Entities/Skeletons/NecromancerEnemy.java
+++ BoneForgeDefense/src/BoneForgeDefense/Entities/Skeletons/NecromancerEnemy.java
@@ -1,18 +1,55 @@
 package BoneForgeDefense.Entities.Skeletons;
 
 import BoneForgeDefense.Entities.Skeleton;
+import BoneForgeDefense.Scenes.LevelOneController;
 
 public class NecromancerEnemy extends Skeleton{
-
-	public NecromancerEnemy(double xPos, double yPos, String spritePath) {
+	private double castTime = 10.0;
+	private double castRange = 5;
+	private static String spritePath = "/BoneForgeDefense/Sprites/necromancer.png";
+	
+	public NecromancerEnemy(double xPos, double yPos) {
 		super(xPos, yPos, spritePath);
-		// TODO Auto-generated constructor stub
+		health = 500;
+		moveSpeed=0.5;
+		boneReward=100;
+		
 	}
 
-	@Override
-	public int getBoneReward() {
-		// TODO Auto-generated method stub
-		return 20;
+	
+	public void castRessurrect() {
+		
+	}
+	
+	private double getBonesInRange(){
+		double bonesInRange = 0;
+		int minX = 0;
+		int minY = 0;
+		int maxX = LevelOneController.MAP_COLS-1;
+		int maxY = LevelOneController.MAP_ROWS-1;
+		
+		if(Math.floor(this.xPos - castRange) >= 0) {
+			minX = (int) Math.floor(this.xPos - castRange);
+		}
+		if(Math.floor(this.xPos + castRange) <= LevelOneController.MAP_COLS-1) {
+			maxX = (int) Math.floor(this.xPos + castRange);
+		}
+		if(Math.floor(this.yPos - castRange) >= 0) {
+			minY = (int) Math.floor(this.yPos - castRange);
+		}
+		if(Math.floor(this.yPos + castRange) <= LevelOneController.MAP_ROWS-1) {
+			maxY = (int) Math.floor(this.yPos + castRange);
+		}
+		
+		
+		for(int i = minY; i<=maxY; i++) {
+			for(int j = minX; j<=maxX; j++) {
+				if (LevelOneController.mapNodes[i][j].getRange(this.xPos,this.yPos)<=(this.castRange*this.castRange)) {
+					bonesInRange+=LevelOneController.mapNodes[i][j].getBones();
+				}
+			}
+		}
+		return bonesInRange;
 	}
 
 }
